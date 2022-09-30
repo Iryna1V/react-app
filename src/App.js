c18871b4dd6fcfeb19f5abc4e1668b7df8591a5a
Iryna1V <ivhaievska@gmail.com>
2022-09-30 16:14:10 +0200

    App React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          {" "}
+          <a
+            className="App-link"
+            href="https://github.com/Iryna1V/WeatherApp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Coded by Iryna Haievska 
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
